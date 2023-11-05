be4fd7d2caadbc44ec226badf8973c135c49c4c5
vladbortnik <vladbortnik@icloud.com>
2023-11-05 06:28:22 -0500

    end sessions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -9,32 +10,48 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 def home():
 	return render_template("index.html")
 
+# @app.route("/<name>")
+# def userless(name):
+# 	return f"<h3>Hello {name}, what are you doing here?</h3>" 
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
 	if request.method == "POST":
 		session.permanent = True
 		user = request.form["nm"]
 		session["user"] = user
+		flash("Login Successful!")
 		return redirect(url_for("user"))
 	else:
 		if "user" in session:
+			flash("Already Logged In!")
 			return redirect(url_for("user"))
 
 		return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods=["POST", "GET"])
 def user():
+	email = None
 	if "user" in session:
-		user = session["user"]
-		return f"<h1>{user}</h1>"
+		user = session.get("user")
+
+		if request.method == "POST":
+			email = request.form["email"]
+			session["email"] = email
+			flash("Email was saved!")
+		else:
+			if email in session:
+				email = session.get("email")
+
+		return render_template("user.html", email=email)
 	else:
 		return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
 	session.pop("user", None)
+	session.pop("email", None)
 	return redirect(url_for("login"))
 
 if __name__ == "__main__":
 	app.run(debug=True)
- 
